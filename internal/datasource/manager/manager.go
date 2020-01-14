13d026639dacf57b24570fffb12b17364f223631
Negasus <negasus@gmail.com>
2020-01-14 12:55:21 +0300

    add clickhouse module

--- internal/datasource/manager/manager.go
+++ internal/datasource/manager/manager.go
@@ -2,19 +2,36 @@ package manager
 
 import (
 	"github.com/balerter/balerter/internal/config"
+	"github.com/balerter/balerter/internal/datasource/provider/clickhouse"
 	"github.com/balerter/balerter/internal/modules"
+	"go.uber.org/zap"
 )
 
 type Manager struct {
+	logger  *zap.Logger
+	modules map[string]modules.Module
 }
 
-func New() *Manager {
-	m := &Manager{}
+func New(logger *zap.Logger) *Manager {
+	m := &Manager{
+		logger:  logger,
+		modules: make(map[string]modules.Module),
+	}
 
 	return m
 }
 
 func (m *Manager) Init(cfg config.DataSources) error {
+
+	for _, clickhouseCfg := range cfg.Clickhouse {
+		module, err := clickhouse.New(clickhouseCfg)
+		if err != nil {
+			return err
+		}
+		m.modules[module.Name()] = module
+
+	}
+
 	return nil
 }
 
--- internal/datasource/provider/clickhouse/clickhouse.go
+++ internal/datasource/provider/clickhouse/clickhouse.go
@@ -1,4 +1,30 @@
 package clickhouse
 
+import (
+	"github.com/balerter/balerter/internal/config"
+	lua "github.com/yuin/gopher-lua"
+)
+
 type Clickhouse struct {
+	name string
+}
+
+func New(cfg config.DataSourceClickhouse) (*Clickhouse, error) {
+	c := &Clickhouse{
+		name: cfg.Name,
+	}
+
+	return c, nil
+}
+
+func (m *Clickhouse) Name() string {
+	return m.name
+}
+
+func (m *Clickhouse) GetLoader() lua.LGFunction {
+	return m.loader
+}
+
+func (m *Clickhouse) loader(L *lua.LState) int {
+	return 0
 }
