ef992396cddba240fb53a3e3fc11bbd4c85540af
Negasus <negasus@gmail.com>
2020-01-15 19:48:21 +0300

    add slack text field for notify message

--- internal/alert/slack/message.go
+++ internal/alert/slack/message.go
@@ -1,40 +1,40 @@
 package slack
 
 type slackMessage struct {
+	Text   string  `json:"text"`
 	Blocks []block `json:"blocks"`
 }
 
 type block struct {
-	Type    string                 `json:"type"`
-	Section *blockSection          `json:"text,omitempty"`
-	Context *[]blockContextElement `json:"elements,omitempty"`
+	Type    string       `json:"type"`
+	Section *blockText   `json:"text,omitempty"`
+	Context *[]blockText `json:"elements,omitempty"`
 }
 
-type blockContextElement struct {
-	Type string `json:"type"`
-	Text string `json:"text"`
-}
-
-type blockSection struct {
-	Type string `json:"type"`
-	Text string `json:"text"`
+type blockText struct {
+	Type  string `json:"type"`
+	Emoji bool   `json:"emoji,omitempty"`
+	Text  string `json:"text"`
 }
 
 func createSlackMessage(alertName, alertText string) slackMessage {
-	s := slackMessage{Blocks: []block{
-		{
-			Type: "section",
-			Section: &blockSection{
-				Type: "mrkdwn",
-				Text: alertText,
+	s := slackMessage{
+		Text: alertText,
+		Blocks: []block{
+			{
+				Type: "section",
+				Section: &blockText{
+					Type: "mrkdwn",
+					Text: alertText,
+				},
 			},
 		},
-	}}
+	}
 
 	if alertName != "" {
 		s.Blocks = append(s.Blocks, block{
 			Type: "context",
-			Context: &[]blockContextElement{
+			Context: &[]blockText{
 				{
 					Type: "mrkdwn",
 					Text: "Alert name: " + alertName,
