c04e88fe6baa10768d0cae144404f84eb5bd7b45
Negasus <negasus@gmail.com>
2020-01-17 14:49:23 +0300

    add test TestScript_ParseMeta_WrongDuration

--- internal/script/script/script_test.go
+++ internal/script/script/script_test.go
@@ -29,3 +29,14 @@ print
 	assert.True(t, s.Ignore)
 	assert.Equal(t, time.Minute*6, s.Interval)
 }
+
+func TestScript_ParseMeta_WrongDuration(t *testing.T) {
+	s := &Script{
+		Body: []byte(`-- @interval 5sm`),
+	}
+
+	err := s.ParseMeta()
+
+	require.Error(t, err)
+	assert.Equal(t, "time: unknown unit sm in duration 5sm", err.Error())
+}
