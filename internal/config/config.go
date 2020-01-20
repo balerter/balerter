bc82621b03556ce1741fca01e0d9248e9222138d
Negasus <negasus@gmail.com>
2020-01-20 13:09:53 +0300

    add config global.api

--- internal/config/config.go
+++ internal/config/config.go
@@ -30,6 +30,11 @@ type Config struct {
 type Global struct {
 	SendStartNotification []string `json:"send_start_notification" yaml:"send_start_notification"`
 	SendStopNotification  []string `json:"send_stop_notification" yaml:"send_stop_notification"`
+	API                   API      `json:"api" yaml:"api"`
+}
+
+type API struct {
+	Address string `json:"address" yaml:"address"`
 }
 
 type Channels struct {
