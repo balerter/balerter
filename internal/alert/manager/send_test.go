ef80d4f792bc8a2dc30eab3ed866ea3548c8c789
Negasus <negasus@gmail.com>
2020-02-04 14:30:10 +0300

    add tests 'contains'

--- internal/alert/manager/send_test.go
+++ internal/alert/manager/send_test.go
@@ -1,73 +1,47 @@
 package manager
 
-//func TestManager_Send(t *testing.T) {
-//	ch1 := &alertChannelMock{}
-//	ch2 := &alertChannelMock{}
-//
-//	ch1.On("Send", mock.Anything, mock.Anything, mock.Anything).Return(nil)
-//
-//	core, logs := observer.New(zap.DebugLevel)
-//	logger := zap.New(core)
-//
-//	m := New(logger)
-//	m.channels["ch1"] = ch1
-//	m.channels["ch2"] = ch2
-//
-//	err := m.Send("message", []string{"ch1"})
-//	require.NoError(t, err)
-//
-//	require.Equal(t, 0, logs.Len())
-//
-//	ch1.AssertCalled(t, "Send", mock.Anything)
-//	ch2.AssertNotCalled(t, "Send", mock.Anything)
-//
-//	logger.Check(zap.ErrorLevel, "")
-//
-//	ch1.AssertExpectations(t)
-//	ch2.AssertExpectations(t)
-//}
-//
-//func TestManager_Send_WrongChannel(t *testing.T) {
-//	ch1 := &alertChannelMock{}
-//
-//	core, logs := observer.New(zap.DebugLevel)
-//	logger := zap.New(core)
-//
-//	m := New(logger)
-//	m.channels["ch1"] = ch1
-//
-//	err := m.Send("message", []string{"ch2"})
-//	require.NoError(t, err)
-//
-//	require.Equal(t, 1, logs.Len())
-//	assert.Equal(t, 1, logs.FilterMessage("channel not found").FilterField(zap.String("channel", "ch2")).Len())
-//
-//	ch1.AssertNotCalled(t, "Send", mock.Anything, mock.Anything)
-//
-//	ch1.AssertExpectations(t)
-//}
-//
-//func TestManager_Send_error(t *testing.T) {
-//	e := fmt.Errorf("error")
-//
-//	ch1 := &alertChannelMock{}
-//	ch1.On("Send", mock.Anything).Return(e)
-//
-//	core, logs := observer.New(zap.DebugLevel)
-//	logger := zap.New(core)
-//
-//	m := New(logger)
-//	m.channels["ch1"] = ch1
-//
-//	err := m.Send("message", []string{"ch1"})
-//	require.NoError(t, err)
-//
-//	require.Equal(t, 1, logs.Len())
-//	assert.Equal(t, 1, logs.FilterMessage("error send message").FilterField(zap.String("channel", "ch1")).FilterField(zap.Error(e)).Len())
-//
-//	ch1.AssertCalled(t, "Send", mock.Anything)
-//
-//	logger.Check(zap.ErrorLevel, "")
-//
-//	ch1.AssertExpectations(t)
-//}
+import "testing"
+
+func Test_contains(t *testing.T) {
+	type args struct {
+		v   string
+		arr []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "empty array",
+			args: args{
+				v:   "foo",
+				arr: nil,
+			},
+			want: false,
+		},
+		{
+			name: "not exists",
+			args: args{
+				v:   "foo",
+				arr: []string{"bar"},
+			},
+			want: false,
+		},
+		{
+			name: "exists",
+			args: args{
+				v:   "foo",
+				arr: []string{"bar", "foo", "baz"},
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := contains(tt.args.v, tt.args.arr); got != tt.want {
+				t.Errorf("contains() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
