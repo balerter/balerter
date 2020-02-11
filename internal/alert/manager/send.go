fb3abd27c57d0272d8d0a6425c22fe4723c9037b
Negasus <negasus@gmail.com>
2020-02-11 18:09:36 +0300

    refactoring send alert function

--- internal/alert/manager/send.go
+++ internal/alert/manager/send.go
@@ -7,23 +7,29 @@ import (
 )
 
 func (m *Manager) Send(level alert.Level, alertName, text string, channels []string, fields []string) {
-	for name, module := range m.channels {
-		if len(channels) > 0 && !contains(name, channels) {
-			m.logger.Debug("skip send message to channel", zap.String("channel name", name))
-			continue
-		}
+	chs := make(map[string]alertChannel)
 
+	if len(channels) > 0 {
+		for _, channelName := range channels {
+			ch, ok := m.channels[channelName]
+			if !ok {
+				m.logger.Error("channel not found", zap.String("channel name", channelName))
+				continue
+			}
+			chs[channelName] = ch
+		}
+	} else {
+		chs = m.channels
+	}
+
+	if len(chs) == 0 {
+		m.logger.Error("empty channels")
+		return
+	}
+
+	for name, module := range chs {
 		if err := module.Send(level, message.New(alertName, text, fields)); err != nil {
 			m.logger.Error("error send message to channel", zap.String("channel name", name), zap.Error(err))
 		}
 	}
-}
-
-func contains(v string, arr []string) bool {
-	for _, item := range arr {
-		if item == v {
-			return true
-		}
-	}
-	return false
 }
--- internal/alert/manager/send_test.go
+++ internal/alert/manager/send_test.go
@@ -10,50 +10,6 @@ import (
 	"testing"
 )
 
-func Test_contains(t *testing.T) {
-	type args struct {
-		v   string
-		arr []string
-	}
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		{
-			name: "empty array",
-			args: args{
-				v:   "foo",
-				arr: nil,
-			},
-			want: false,
-		},
-		{
-			name: "not exists",
-			args: args{
-				v:   "foo",
-				arr: []string{"bar"},
-			},
-			want: false,
-		},
-		{
-			name: "exists",
-			args: args{
-				v:   "foo",
-				arr: []string{"bar", "foo", "baz"},
-			},
-			want: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := contains(tt.args.v, tt.args.arr); got != tt.want {
-				t.Errorf("contains() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestManager_Send_no_channels(t *testing.T) {
 	core, logs := observer.New(zap.DebugLevel)
 	logger := zap.New(core)
