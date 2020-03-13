fa036b5f6e930127938bbd0aaf035dcd67672fa8
Negasus <negasus@gmail.com>
2020-03-13 17:20:07 +0300

    add stubs Marshal/Unmarshal for alert.Alert

---
+++ internal/alert/alert/marshal.go
@@ -0,0 +1,14 @@
+package alert
+
+func (a *Alert) Marshal() []byte {
+	res := make([]byte, 0)
+	panic("not implemented") // todo
+	return res
+}
+
+func (a *Alert) Unmarshal(src []byte, dest *Alert) error {
+	//res := make([]byte, 0)
+	panic("not implemented") // todo
+
+	return nil
+}
