a8ec08e26a2128055d96c77cfbdee8ef1bc26a5e
Negasus <negasus@gmail.com>
2020-03-17 14:06:59 +0300

    add test for config validation: core storages

--- internal/config/storages_core.go
+++ internal/config/storages_core.go
@@ -1,14 +1,22 @@
 package config
 
+import "fmt"
+
 type StoragesCore struct {
 	File []StorageCoreFile `json:"file" yaml:"file"`
 }
 
 func (cfg StoragesCore) Validate() error {
+	var names []string
+
 	for _, c := range cfg.File {
+		names = append(names, c.Name)
 		if err := c.Validate(); err != nil {
 			return err
 		}
+	}
+	if name := checkUnique(names); name != "" {
+		return fmt.Errorf("found duplicated name for core storages 'file': %s", name)
 	}
 
 	return nil
---
+++ internal/config/storages_core_test.go
@@ -0,0 +1,46 @@
+package config
+
+import "testing"
+
+func TestStoragesCore_Validate(t *testing.T) {
+	type fields struct {
+		File []StorageCoreFile
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+		errText string
+	}{
+		{
+			name: "duplicated file",
+			fields: fields{
+				File: []StorageCoreFile{{Name: "a", Path: "a"}, {Name: "a", Path: "a"}},
+			},
+			wantErr: true,
+			errText: "found duplicated name for core storages 'file': a",
+		},
+		{
+			name: "ok",
+			fields: fields{
+				File: []StorageCoreFile{{Name: "a", Path: "a"}, {Name: "a2", Path: "a"}},
+			},
+			wantErr: false,
+			errText: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cfg := StoragesCore{
+				File: tt.fields.File,
+			}
+			err := cfg.Validate()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if err != nil && err.Error() != tt.errText {
+				t.Errorf("Validate() error = '%s', wantErrText '%s'", err.Error(), tt.errText)
+			}
+		})
+	}
+}
