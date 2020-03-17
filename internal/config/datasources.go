124742263027e2518631c5c3624c8997a3dc810a
Negasus <negasus@gmail.com>
2020-03-17 13:14:42 +0300

    add test for config validation: datasource

--- internal/config/datasources.go
+++ internal/config/datasources.go
@@ -1,5 +1,7 @@
 package config
 
+import "fmt"
+
 type DataSources struct {
 	Clickhouse []DataSourceClickhouse `json:"clickhouse" yaml:"clickhouse"`
 	Prometheus []DataSourcePrometheus `json:"prometheus" yaml:"prometheus"`
@@ -9,30 +11,60 @@ type DataSources struct {
 }
 
 func (cfg DataSources) Validate() error {
+	var names []string
+
 	for _, c := range cfg.Clickhouse {
+		names = append(names, c.Name)
 		if err := c.Validate(); err != nil {
 			return err
 		}
 	}
+	if name := checkUnique(names); name != "" {
+		return fmt.Errorf("found duplicated name for datasource 'clickhouse': %s", name)
+	}
+
+	names = names[:0]
 	for _, c := range cfg.Prometheus {
+		names = append(names, c.Name)
 		if err := c.Validate(); err != nil {
 			return err
 		}
 	}
+	if name := checkUnique(names); name != "" {
+		return fmt.Errorf("found duplicated name for datasource 'prometheus': %s", name)
+	}
+
+	names = names[:0]
 	for _, c := range cfg.Postgres {
+		names = append(names, c.Name)
 		if err := c.Validate(); err != nil {
 			return err
 		}
 	}
+	if name := checkUnique(names); name != "" {
+		return fmt.Errorf("found duplicated name for datasource 'postgres': %s", name)
+	}
+
+	names = names[:0]
 	for _, c := range cfg.MySQL {
+		names = append(names, c.Name)
 		if err := c.Validate(); err != nil {
 			return err
 		}
 	}
+	if name := checkUnique(names); name != "" {
+		return fmt.Errorf("found duplicated name for datasource 'mysql': %s", name)
+	}
+
+	names = names[:0]
 	for _, c := range cfg.Loki {
+		names = append(names, c.Name)
 		if err := c.Validate(); err != nil {
 			return err
 		}
+	}
+	if name := checkUnique(names); name != "" {
+		return fmt.Errorf("found duplicated name for datasource 'loki': %s", name)
 	}
 
 	return nil
