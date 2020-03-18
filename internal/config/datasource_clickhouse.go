87136e6a696993a920bdff3ec99fc5c0d08dda8c
Negasus <negasus@gmail.com>
2020-03-18 17:09:27 +0300

    add missed annotations

--- internal/config/datasource_clickhouse.go
+++ internal/config/datasource_clickhouse.go
@@ -7,14 +7,14 @@ import (
 )
 
 type DataSourceClickhouse struct {
-	Name        string `json:"name" yaml:"name"`
-	Host        string `json:"host" yaml:"host"`
-	Port        int    `json:"port" yaml:"port"`
-	Username    string `json:"username" yaml:"username"`
-	Password    string `json:"password" yaml:"password"`
-	Database    string `json:"database" yaml:"database"`
-	SSLCertPath string `json:"sslCertPath" yaml:"sslCertPath"`
-	Timeout     time.Duration
+	Name        string        `json:"name" yaml:"name"`
+	Host        string        `json:"host" yaml:"host"`
+	Port        int           `json:"port" yaml:"port"`
+	Username    string        `json:"username" yaml:"username"`
+	Password    string        `json:"password" yaml:"password"`
+	Database    string        `json:"database" yaml:"database"`
+	SSLCertPath string        `json:"sslCertPath" yaml:"sslCertPath"`
+	Timeout     time.Duration `json:"timeout" yaml:"timeout"`
 }
 
 func (cfg DataSourceClickhouse) Validate() error {
