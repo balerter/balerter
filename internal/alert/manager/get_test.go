0582208d34b7f6e2495f199d8c1daa8a174e8348
Negasus <negasus@gmail.com>
2020-04-14 23:02:46 +0300

    add tests

--- internal/alert/manager/get_test.go
+++ internal/alert/manager/get_test.go
@@ -1,7 +1,11 @@
 package manager
 
 import (
+	"fmt"
+	"github.com/balerter/balerter/internal/alert/alert"
+	coreStorage "github.com/balerter/balerter/internal/core_storage"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	lua "github.com/yuin/gopher-lua"
 	"testing"
 )
@@ -60,4 +64,61 @@ func TestGet_AlertNameEmptyString(t *testing.T) {
 
 	assert.Equal(t, lua.LTNil, e1.Type())
 	assert.Equal(t, "alert name must be not empty", e2.String())
+}
+
+func TestGet_ErrorGet(t *testing.T) {
+	mck := coreStorage.NewMock("")
+	mck.AlertMock().On("Get", "foo").Return(nil, fmt.Errorf("error1"))
+
+	m := &Manager{
+		engine: mck,
+	}
+
+	f := m.get(nil)
+
+	L := lua.NewState()
+	L.Push(lua.LString("foo"))
+
+	n := f(L)
+
+	assert.Equal(t, 2, n)
+
+	e1 := L.Get(2)
+	e2 := L.Get(3)
+
+	assert.Equal(t, lua.LTNil, e1.Type())
+	assert.Equal(t, "error get alert: error1", e2.String())
+}
+
+func TestGet(t *testing.T) {
+	a := &alert.Alert{}
+	a.SetName("bar")
+	a.UpdateLevel(alert.LevelError)
+	a.Inc()
+
+	mck := coreStorage.NewMock("")
+	mck.AlertMock().On("Get", "foo").Return(a, nil)
+
+	m := &Manager{
+		engine: mck,
+	}
+
+	f := m.get(nil)
+
+	L := lua.NewState()
+	L.Push(lua.LString("foo"))
+
+	n := f(L)
+
+	assert.Equal(t, 1, n)
+
+	e1 := L.Get(2)
+
+	require.Equal(t, lua.LTTable, e1.Type())
+
+	e2 := e1.(*lua.LTable)
+
+	assert.Equal(t, "bar", e2.RawGetString("name").String())
+	assert.Equal(t, "error", e2.RawGetString("level").String())
+	assert.Equal(t, "1", e2.RawGetString("count").String())
 }
