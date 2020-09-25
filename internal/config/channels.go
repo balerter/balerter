1f28e01277378d24a2b3a7adadf1ed8ea40f647d
Negasus <negasus@gmail.com>
2020-09-25 17:58:48 +0300

    go fmt

--- internal/config/channels.go
+++ internal/config/channels.go
@@ -3,13 +3,13 @@ package config
 import "fmt"
 
 type Channels struct {
-	Email    []*ChannelEmail    `json:"email" yaml:"email"`
-	Slack    []*ChannelSlack    `json:"slack" yaml:"slack"`
-	Telegram []*ChannelTelegram `json:"telegram" yaml:"telegram"`
-	Syslog   []*ChannelSyslog   `json:"syslog" yaml:"syslog"`
-	Notify   []*ChannelNotify   `json:"notify" yaml:"notify"`
-	Discord  []*ChannelDiscord  `json:"discord" yaml:"discord"`
-	Webhook  []*ChannelWebhook  `json:"webhook" yaml:"webhook"`
+	Email        []*ChannelEmail        `json:"email" yaml:"email"`
+	Slack        []*ChannelSlack        `json:"slack" yaml:"slack"`
+	Telegram     []*ChannelTelegram     `json:"telegram" yaml:"telegram"`
+	Syslog       []*ChannelSyslog       `json:"syslog" yaml:"syslog"`
+	Notify       []*ChannelNotify       `json:"notify" yaml:"notify"`
+	Discord      []*ChannelDiscord      `json:"discord" yaml:"discord"`
+	Webhook      []*ChannelWebhook      `json:"webhook" yaml:"webhook"`
 	Alertmanager []*ChannelAlertmanager `json:"alertmanager" yaml:"alertmanager"`
 }
 
@@ -90,10 +90,10 @@ func (cfg *Channels) Validate() error { //nolint:gocyclo // Validate calls only 
 	}
 	if name := checkUnique(names); name != "" {
 		return fmt.Errorf("found duplicated name for channels 'webhook': %s", name)
-  }
-  
+	}
+
 	names = names[:0]
-  for _, c := range cfg.Alertmanager {
+	for _, c := range cfg.Alertmanager {
 		names = append(names, c.Name)
 		if err := c.Validate(); err != nil {
 			return fmt.Errorf("validate channel alertmanager: %w", err)
