116cf74f7fb7bf964d759edcd6fe8535818b643d
Negasus <negasus@gmail.com>
2020-12-14 18:39:39 +0300

    fix tests

--- internal/am/get_test.go
+++ internal/am/get_test.go
@@ -1,30 +1,35 @@
 package manager
 
 import (
+	"fmt"
+	"github.com/balerter/balerter/internal/corestorage"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestGet_empty_name(t *testing.T) {
-	//m := &Manager{}
-	//
-	//_, err := m.Get(" ")
-	//
-	//require.Error(t, err)
-	//assert.Equal(t, "empty alert name", err.Error())
+	m := &Manager{}
+
+	_, err := m.Get(" ")
+
+	require.Error(t, err)
+	assert.Equal(t, "empty alert name", err.Error())
 }
 
 func TestGet_error(t *testing.T) {
-	//e1 := fmt.Errorf("e1")
-	//
-	//mockStorage := corestorage.NewMock("m")
-	//mockStorage.AlertMock().On("Get", mock.Anything).Return(e1)
-	//
-	//m := &Manager{
-	//	storage: mockStorage,
-	//}
-	//
-	//_, err := m.Get("foo")
-	//
-	//require.Error(t, err)
-	//assert.Equal(t, "empty alert name", err.Error())
+	e1 := fmt.Errorf("e1")
+
+	mockStorage := corestorage.NewMock("m")
+	mockStorage.AlertMock().On("Get", mock.Anything).Return(nil, e1)
+
+	m := &Manager{
+		storage: mockStorage,
+	}
+
+	_, err := m.Get("foo")
+
+	require.Error(t, err)
+	assert.Equal(t, "error get alert, e1", err.Error())
 }
