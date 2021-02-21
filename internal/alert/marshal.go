1b6db95cf5c0a8dc196437cee5cadc0a971ae8c1
Negasus <negasus@gmail.com>
2021-02-21 22:27:50 +0300

    remove alert.Marshal binary, make Marshal json. Add 'Alerts' type (alerts slice)

--- internal/alert/marshal.go
+++ internal/alert/marshal.go
@@ -1,120 +1,25 @@
 package alert
 
 import (
-	"encoding/binary"
-	"errors"
+	"bytes"
+	"fmt"
+	"time"
 )
 
-const (
-	timeFieldBinarySize = 15
-)
+func (a *Alert) Marshal() []byte {
+	pattern := `{"name":"%s","level":"%s","level_num":%d,"count":%d,"last_change":"%s","start":"%s"}`
 
-var (
-	ErrDecodeAlertName = errors.New("error decode alert Name")
-	ErrSourceTooSmall  = errors.New("source too small")
-	ErrDecodeLevel     = errors.New("error decode Level")
-	ErrDecodeCount     = errors.New("error decode Count")
-	ErrSourceTooLong   = errors.New("source too long")
-)
-
-// Marshal an Alert to the byte slice
-func (a *Alert) Marshal() ([]byte, error) {
-	res := make([]byte, 0)
-
-	buf := make([]byte, 64)
-	var n int
-
-	// Name
-	n = binary.PutUvarint(buf, uint64(len(a.Name)))
-	res = append(res, buf[:n]...)
-	res = append(res, a.Name...)
-
-	// Level
-	n = binary.PutUvarint(buf, uint64(a.Level))
-	res = append(res, buf[:n]...)
-
-	// LastChange
-	t, err := a.LastChange.MarshalBinary()
-	if err != nil {
-		return nil, err
-	}
-	res = append(res, t...)
-
-	// Start
-	t, err = a.Start.MarshalBinary()
-	if err != nil {
-		return nil, err
-	}
-	res = append(res, t...)
-
-	// Count
-	n = binary.PutUvarint(buf, uint64(a.Count))
-	res = append(res, buf[:n]...)
-
-	return res, nil
+	return []byte(fmt.Sprintf(pattern, a.Name, a.Level.String(), a.Level, a.Count, a.LastChange.Format(time.RFC3339), a.Start.Format(time.RFC3339)))
 }
 
-// Unmarshal the byte slice to an Alert
-func (a *Alert) Unmarshal(src []byte) error {
-	// Name length
-	l, n := binary.Uvarint(src)
-	if n <= 0 {
-		return ErrDecodeAlertName
-	}
-	src = src[n:]
+func (a Alerts) Marshal() []byte {
+	bufs := make([][]byte, 0)
 
-	// Name
-	if len(src) < int(l) {
-		return ErrSourceTooSmall
-	}
-	a.Name = string(src[:l])
-	src = src[l:]
-
-	// Level
-	if len(src) == 0 {
-		return ErrSourceTooSmall
-	}
-	l, n = binary.Uvarint(src)
-	if n <= 0 {
-		return ErrDecodeLevel
-	}
-	a.Level = Level(l)
-	src = src[n:]
-
-	// LastChange
-	if len(src) < timeFieldBinarySize {
-		return ErrSourceTooSmall
-	}
-	err := a.LastChange.UnmarshalBinary(src[:timeFieldBinarySize])
-	if err != nil {
-		return err
-	}
-	src = src[timeFieldBinarySize:]
-
-	// Start
-	if len(src) < timeFieldBinarySize {
-		return ErrSourceTooSmall
-	}
-	err = a.Start.UnmarshalBinary(src[:timeFieldBinarySize])
-	if err != nil {
-		return err
-	}
-	src = src[timeFieldBinarySize:]
-
-	// Count
-	if len(src) == 0 {
-		return ErrSourceTooSmall
-	}
-	l, n = binary.Uvarint(src)
-	if n <= 0 {
-		return ErrDecodeCount
-	}
-	a.Count = int(l)
-	src = src[n:]
-
-	if len(src) > 0 {
-		return ErrSourceTooLong
+	for _, alert := range a {
+		bufs = append(bufs, alert.Marshal())
 	}
 
-	return nil
+	buf := bytes.Join(bufs, []byte{','})
+
+	return append([]byte{'['}, append(buf, ']')...)
 }
