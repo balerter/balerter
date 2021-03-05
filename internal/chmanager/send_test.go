69b633719c05159e366d79f22ea5a2b9a1f7472f
Negasus <negasus@gmail.com>
2021-03-05 18:10:02 +0300

    add tests

--- internal/chmanager/send_test.go
+++ internal/chmanager/send_test.go
@@ -1,8 +1,14 @@
 package manager
 
 import (
+	"fmt"
+	"github.com/balerter/balerter/internal/alert"
 	"github.com/balerter/balerter/internal/message"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zaptest/observer"
+	"testing"
 )
 
 type alertChannelMock struct {
@@ -19,72 +25,79 @@ func (m *alertChannelMock) Send(mes *message.Message) error {
 	return args.Error(0)
 }
 
-//func TestManager_Send_no_channels(t *testing.T) {
-//	m := &ChannelsManager{
-//		channels: map[string]alertChannel{},
-//	}
-//
-//	err := m.Send(alert.New("alertName"), "alertText", &alert.Options{})
-//	require.Error(t, err)
-//	assert.Equal(t, ErrEmptyChannels, err)
-//}
-//
-//func TestManager_Send_channel_not_found(t *testing.T) {
-//	chan1 := &alertChannelMock{}
-//
-//	m := &ChannelsManager{
-//		channels: map[string]alertChannel{
-//			"chan1": chan1,
-//		},
-//	}
-//
-//	err := m.Send("", "alertName", "alertText", &alert.Options{Channels: []string{"chan2"}}, nil)
-//	require.Error(t, err)
-//	assert.Equal(t, ErrEmptyChannels, err)
-//
-//	chan1.AssertNotCalled(t, "Send", mock.Anything)
-//
-//	chan1.AssertExpectations(t)
-//}
-//
-//func TestManager_Send_ok(t *testing.T) {
-//	core, logs := observer.New(zap.DebugLevel)
-//	logger := zap.New(core)
-//
-//	chan1 := &alertChannelMock{}
-//	chan1.On("Send", mock.Anything).Return(nil)
-//
-//	m := &ChannelsManager{
-//		logger: logger,
-//		channels: map[string]alertChannel{
-//			"chan1": chan1,
-//		},
-//	}
-//
-//	err := m.Send("", "alertName", "alertText", &alert.Options{}, nil)
-//	require.NoError(t, err)
-//
-//	chan1.AssertCalled(t, "Send", mock.Anything)
-//
-//	assert.Equal(t, 0, logs.Len())
-//
-//	chan1.AssertExpectations(t)
-//}
-//
-//func TestManager_Send_error(t *testing.T) {
-//	chan1 := &alertChannelMock{}
-//	chan1.On("Send", mock.Anything).Return(fmt.Errorf("err1"))
-//
-//	m := &ChannelsManager{
-//		channels: map[string]alertChannel{
-//			"chan1": chan1,
-//		},
-//	}
-//
-//	err := m.Send("", "alertName", "alertText", &alert.Options{}, nil)
-//	require.NoError(t, err)
-//
-//	chan1.AssertCalled(t, "Send", mock.Anything)
-//
-//	chan1.AssertExpectations(t)
-//}
+func TestManager_Send_no_channels(t *testing.T) {
+	core, logger := observer.New(zap.DebugLevel)
+	m := &ChannelsManager{
+		channels: map[string]alertChannel{},
+		logger:   zap.New(core),
+	}
+
+	m.Send(alert.New("alertName"), "alertText", &alert.Options{})
+
+	assert.Equal(t, 1, logger.FilterMessage("the message was not sent, empty channels").Len())
+}
+
+func TestManager_Send_channel_not_found(t *testing.T) {
+	core, logger := observer.New(zap.DebugLevel)
+	chan1 := &alertChannelMock{}
+
+	m := &ChannelsManager{
+		channels: map[string]alertChannel{
+			"chan1": chan1,
+		},
+		logger: zap.New(core),
+	}
+
+	m.Send(alert.New("alertName"), "alertText", &alert.Options{Channels: []string{"chan2"}})
+
+	chan1.AssertNotCalled(t, "Send", mock.Anything)
+
+	chan1.AssertExpectations(t)
+
+	assert.Equal(t, 1, logger.FilterMessage("the message was not sent, empty channels").Len())
+	assert.Equal(t, 1, logger.FilterMessage("channel not found").Len())
+}
+
+func TestManager_Send_ok(t *testing.T) {
+	core, logs := observer.New(zap.DebugLevel)
+	logger := zap.New(core)
+
+	chan1 := &alertChannelMock{}
+	chan1.On("Send", mock.Anything).Return(nil)
+
+	m := &ChannelsManager{
+		logger: logger,
+		channels: map[string]alertChannel{
+			"chan1": chan1,
+		},
+	}
+
+	m.Send(alert.New("alertName"), "alertText", &alert.Options{})
+
+	chan1.AssertCalled(t, "Send", mock.Anything)
+
+	assert.Equal(t, 0, logs.Len())
+
+	chan1.AssertExpectations(t)
+}
+
+func TestManager_Send_error(t *testing.T) {
+	chan1 := &alertChannelMock{}
+	chan1.On("Send", mock.Anything).Return(fmt.Errorf("err1"))
+
+	core, logger := observer.New(zap.DebugLevel)
+	m := &ChannelsManager{
+		channels: map[string]alertChannel{
+			"chan1": chan1,
+		},
+		logger: zap.New(core),
+	}
+
+	m.Send(alert.New("alertName"), "alertText", &alert.Options{})
+
+	chan1.AssertCalled(t, "Send", mock.Anything)
+
+	chan1.AssertExpectations(t)
+
+	assert.Equal(t, 1, logger.FilterMessage("error send the message to the channel").Len())
+}
