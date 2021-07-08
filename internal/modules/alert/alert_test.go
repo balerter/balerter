9745376daf3b821c6a1c4a83717d39c3f359b4da
Andrew Privalov <negasus@gmail.com>
2021-07-08 11:38:12 +0300

    add module alert tests (#48)

---
+++ internal/modules/alert/alert_test.go
@@ -0,0 +1,54 @@
+package alert
+
+import (
+	"github.com/stretchr/testify/assert"
+	lua "github.com/yuin/gopher-lua"
+	"testing"
+)
+
+func TestModuleName(t *testing.T) {
+	assert.Equal(t, "alert", ModuleName())
+}
+
+func TestMethods(t *testing.T) {
+	assert.Equal(t, []string{
+		"warn",
+		"warning",
+		"error",
+		"fail",
+		"success",
+		"ok",
+		"get",
+	}, Methods())
+}
+
+func TestNew(t *testing.T) {
+	a := New(nil, nil, nil)
+	assert.IsType(t, &Alert{}, a)
+}
+
+func TestName(t *testing.T) {
+	a := &Alert{}
+	assert.Equal(t, "alert", a.Name())
+}
+
+func TestAlert_GetLoader(t *testing.T) {
+	a := &Alert{}
+
+	f := a.GetLoader(nil)
+
+	ls := lua.NewState()
+	n := f(ls)
+	assert.Equal(t, 1, n)
+
+	v := ls.Get(1).(*lua.LTable)
+
+	for _, m := range Methods() {
+		assert.Equal(t, lua.LTFunction, v.RawGetString(m).Type())
+	}
+}
+
+func TestStop(t *testing.T) {
+	a := &Alert{}
+	assert.NoError(t, a.Stop())
+}
