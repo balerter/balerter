3613b0c362637b13351594608c4903e0e10891f4
Negasus <negasus@gmail.com>
2022-08-11 18:36:33 +0300

    add safemode key for runtime module

--- internal/modules/runtime/coreapi.go
+++ internal/modules/runtime/coreapi.go
@@ -16,12 +16,14 @@ func (m *Runtime) CoreApiHandler(method string, parts []string, params map[strin
 		IsOnce       bool   `json:"is_once"`
 		WithScript   string `json:"with_script"`
 		ConfigSource string `json:"config_source"`
+		SafeMode     bool   `json:"safe_mode"`
 	}{
 		LogLevel:     m.flg.LogLevel,
 		IsDebug:      m.flg.Debug,
 		IsOnce:       m.flg.Once,
 		WithScript:   m.flg.Script,
 		ConfigSource: m.flg.ConfigFilePath,
+		SafeMode:     m.flg.SafeMode,
 	}
 
 	return resp, http.StatusOK, nil
